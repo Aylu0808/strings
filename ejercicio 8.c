ec95d2be3ab214895718efc91351892f7aa83d82
Aylu0808 <105893167+Aylu0808@users.noreply.github.com>
2022-08-05 16:12:39 -0300

    Update ejercicio 8.c

--- ejercicio 8.c
+++ ejercicio 8.c
@@ -4,24 +4,38 @@
 Luego de finalizar el ingreso, mostrar en pantalla cuál es el primer nombre en orden alfabético de todos los ingresados.
 
 ************************************************************************************************************/
-
-#include<stdio.h>
-#include<conio.h>
-#include<string.h>
-
-int main ()
-{
-    char nombres[30];
-
-    for(;;){
-        
-    printf("Ingerse un nombre:");
-    fgets(nombres, 30, stdin);
+#include <stdio.h>
+#include <string.h>
+ 
+int main() {
     
-    nombres[strcspn(nombres, "\r\n")] = 0;
+  char nombre[15];
+  char minimo[15];
+  char mensaje[20]={'F', 'I', 'N', 'A', 'L', 'I', 'Z', 'A', 'D', 'O'};
+  int s=0;
+ 
+  do {
+    printf("Ingrese nombre [FIN] para terminar: ");
+    scanf("%s",nombre);
     
-    if(strcmp(nombres, "FIN") == 0) break;
-    
+    if(strcmp(nombre,"FIN") == 0){
+        strcpy(minimo, mensaje);
     }
+    else{
+        if(s == 0){
+        strcpy(minimo, nombre);
+    }
+    else{
+        if(strcmp(nombre, minimo) < 0)
+        {
+            strcpy(minimo, nombre);
+        }
+    } 
+    }
+    
+    s++;
+  } while (strcmp(nombre,"FIN") != 0);
+    
+    printf("%s",minimo);
  
 }
