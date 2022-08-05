6bc8c38f2801fb01281f3a36313127faa9a54cc8
Aylu0808 <105893167+Aylu0808@users.noreply.github.com>
2022-08-05 08:14:32 -0300

    Update Ejercicio 9.c

--- Ejercicio 9.c
+++ Ejercicio 9.c
@@ -1,36 +1,49 @@
-/******************************************************************************
+/* 
 
 9) El usuario ingresará una palabra de hasta 10 letras. Se desea mostrarla en pantalla pero encriptada según el "Código César".
 Esto consiste en reemplazar cada letra con la tercera consecutiva en el abecedario. Por ejemplo, "CASA" se convierte en "FDVD".
 Tener en cuenta que las últimas letras deben volver al inicio, por ejemplo la Y se convierte B.
-Este mecanismo se utilizaba en el Imperio Romano.
 
-*******************************************************************************/
-#include <stdio.h> //Defino las variables
-#include <stdlib.h>
+*/
+
+#include<stdio.h>//Se declaran las bibliotecas
 #include <string.h>
-#define max 10 // Defino cantidad maxima de letras 
 
+int main(){
+	
+	char pal[20]; //Se declaran las variables
+	char A[20];
+	int i = 0;
+	
+	printf("Ingrese la palabra que se quiere encriptar: "); //Se hace el ingreso de la palabra 
+	scanf("%s", pal);
+	
+	while(pal[i] != '\0') //El bucle se repite hasta que finaliza la palabra.
+	{
+		A[i] = pal[i]+3; 
+		
+		if(pal[i] == 'x'){//Se compara con las ultimas letras para que empiece de nuevo el abecedario
+			A[i] = 'a'; 
+		}
+		if(pal[i] == 'y'){
+			A[i] = 'b';
+		}
+		if(pal[i] == 'z'){
+			A[i] = 'c';
+		}
+		if(pal[i] == 'X'){   
+			A[i] = 'A'; 
+		}
+		if(palabra[i] == 'Y'){
+			A[i] = 'B';
+		}
+		if(pal[i] == 'Z'){
+			A[i] = 'C';
+		}
+		i++;
+	}
+	
+	printf("La palabra encriptada es: %s\n", A); //Se imprime la palabra encriptada.
 
-void encriptacion(char palabra[max], int a) //Creo una funcion para encriptr la palabra
-{
-    int i=0;
-    while(palabra[i]!='\0') //Con esta repeticion se le ira aumentando a cada letra y la mostrara en pantalla
-    {
-        palabra[i]=palabra[i]+a;
-        i++;
-    }
-    printf("La palabra encriptada es: %s", palabra);
 }
-
-int main()
-{
-    int x=3;
-    char pal[max];
-    
-    printf("Palabra que quiere encriptar: ");//Pido ingreso de la palabra
-    scanf("%s", pal);
-    
-    encriptacion(pal, x);//Llamo a la funcion
    
-}
